85c4a35952870dc28a068eb222d67e16626a4e94
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2024-08-31 00:24:50 -0300

    update on activity 13

--- atividades_02/atividade 13.js
+++ atividades_02/atividade 13.js
@@ -1,16 +1,17 @@
-let cart = () => {
-  let shoppingCart = {
-    items:[
-      {name: "Vara de Pesca", quantity: 2, unitPrice: 113.98},
-      {name: "Molinete", quantity: 2, unitPrice: 556.68},
-      {name: "Isca Artificial", quantity: 6, unitPrice: 59.99}
-    ]
-  }
+let shoppingCart = {
+  items:[
+    {name: "Vara de Pesca", quantity: 2, unitPrice: 113.98},
+    {name: "Molinete", quantity: 2, unitPrice: 556.68},
+    {name: "Isca Artificial", quantity: 6, unitPrice: 59.99}
+  ]
+}
+
+let cart = (shoppingCart) => {
   let total = 0;
   shoppingCart.items.forEach(item =>{
     total += item.quantity * item.unitPrice
   })
   return total;
 }
-let totalCart = cart();
+let totalCart = cart(shoppingCart);
 console.log(`Total a pagar: R$ ${totalCart.toFixed(2)}`);
