7b3996ef1d689e221202d6e1a70516058b9245fe
CarlosEduardoVdeOliveira <vieiradeoliveira70@gmail.com>
2024-10-04 15:22:46 -0300

    feat: create app footer, challenge 03

---
+++ atividades_03/.history/assets/js/menuHamburger_20241004144604.js
@@ -0,0 +1,45 @@
+const menuOpen = document.getElementById("btn-menu-hamburger");
+const iconOpen = document.que("btn-menu-hamburger i");
+const nav = document.getElementById("nav-menu");
+console.log(iconOpen);
+
+const classMenu = "fa-bars-staggered"
+
+menuOpen.addEventListener("click", () => {
+  if (nav.className === "header__nav--container") {
+    nav.style.display = "block"
+    nav.classList.add("menu__hamburger--container")
+  }else if (nav.className !== "header__nav--container") {
+    nav.style.display = "none"
+    nav.classList.remove("menu__hamburger--container")
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* navMenuList.classList.toggle("menu__hamburger--container--list-links")
+navMenuList.classList.remove("header__nav--list-links")
+links.forEach(link => link.classList.toggle("menu__hamburger--container--list-item-link"))
+links.forEach(link => link.classList.remove("header__nav--list-item-link")) */
